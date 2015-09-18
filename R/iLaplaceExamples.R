756e8e255397103d8f2712dab10cd8870f8c4efc
ruli <erlis.ruli@gmail.com>
2015-09-18 14:40:32 +0200

    modification at 14:40

---
+++ R/iLaplaceExamples.R
@@ -0,0 +1,18 @@
+#' iLaplaceExamples: A package for examples with iLaplace.
+#'
+#' The iLaplaceExamples package provides utilites for running examples of approximating marignal likelihoods by the improved Laplace method of Ruli et al. (2015), implemented in the package iLaplace. Other approximation method such as the standard Laplace and some Monte Carlo methods are aslo provided. There are five examples:
+#' \enumerate{
+#'  \item Gompertz model
+#'  \item multivariate t/skew-t distribtuion
+#'  \item nonlinear regression models with two datasets
+#'  \item binary logistic regression with random effects
+#'  \item binary logistic regression with crossed random effects with the \code{\link[iLaplaceExamples]{Salamander}} data}
+#' which functions are listed below.
+#' foo, bar and baz.
+#'
+#' @section Functions Example 1:
+#' The foo functions...
+#'
+#' @docType package
+#' @name iLaplaceExtra
+NULL
